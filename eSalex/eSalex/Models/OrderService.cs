764a8758f97d3ba04817a6abda18bad3770e7480
AJWUtw <aj111743@gmail.com>
2017-03-10 18:55:15 +0800

    a.定義Order 資料型別 b.設計資料存取方法

---
+++ eSalex/eSalex/Models/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSalex.Models
+{
+    /// <summary>
+    /// 資料服務
+    /// </summary>
+    public class OrderService
+    {
+        public void InsertOrder(Models.Order order) { }
+
+        public void DeleteOrderById(string id) { }
+
+        public void UpdateOrderById(string id) { }
+
+        public void GetOrderById(string id) { }
+
+        public void List<GetOrderByCondition>() { }
+
+
+    }
+}
